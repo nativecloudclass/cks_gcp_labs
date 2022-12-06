663ebe1b1f83fd6aa053c584221acde728339b99
Ritesh <119970797+nativecloudclass@users.noreply.github.com>
2022-12-07 01:13:46 +0530

    Update setup_resources.sh

--- setup_resources.sh
+++ setup_resources.sh
@@ -21,10 +21,12 @@ https://cloud.google.com/compute/docs/regions-zones
 # INSTALL cks-master
 gcloud compute ssh cks-master
 sudo -i
-bash <(curl -s https://raw.githubusercontent.com/killer-sh/cks-course-environment/master/cluster-setup/latest/install_master.sh)
+curl https://github.com/nativecloudclass/cks_gcp_labs/blob/main/install_master.sh --output install_master.sh
+bash install_master.sh
 
 
 # INSTALL cks-worker
 gcloud compute ssh cks-worker
 sudo -i
-bash <(curl -s https://raw.githubusercontent.com/killer-sh/cks-course-environment/master/cluster-setup/latest/install_worker.sh)
+curl https://github.com/nativecloudclass/cks_gcp_labs/blob/main/install_worker.sh --output install_worker.sh
+bash install_worker.sh
